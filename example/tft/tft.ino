91016a28387e1c5b82e1acd19915156acc4fb65d
Gray Hat Guy LLC <grayhatguy@grayhatguy.com>
2023-01-31 23:01:05 -0500

    ok

--- example/tft/tft.ino
+++ example/tft/tft.ino
@@ -156,66 +156,66 @@ void loop() {
   tft.fillScreen(TFT_BROWN);
   tft.setTextColor(TFT_GREEN);
 
-  tft.drawString(" !\"#$%&'()*+,-./0123456", 0, 0, 2);
-  tft.drawString("789:;<=>?@ABCDEFGHIJKL", 0, 16, 2);
-  tft.drawString("MNOPQRSTUVWXYZ[\\]^_`", 0, 32, 2);
-  tft.drawString("abcdefghijklmnopqrstuvw", 0, 48, 2);
+  tft.drawString(" !\"Applefuckers", 0, 0, 2);
+  tft.drawString("A p p l e f u c k e r s", 0, 16, 2);
+  tft.drawString("apple", 0, 32, 2);
+  tft.drawString("fuckers", 0, 48, 2);
   xpos = 0;
-  xpos += tft.drawString("xyz{|}~", 0, 64, 2);
+  xpos += tft.drawString("#!&$", 0, 64, 2);
   tft.drawChar(127, xpos, 64, 2);
   delay(WAIT);
 
   tft.fillScreen(TFT_BROWN);
   tft.setTextColor(TFT_GREEN);
 
-  tft.drawString(" !\"#$%&'()*+,-.", 0, 0, 4);
-  tft.drawString("/0123456789:;", 0, 26, 4);
-  tft.drawString("<=>?@ABCDE", 0, 52, 4);
-  tft.drawString("FGHIJKLMNO", 0, 78, 4);
-  tft.drawString("PQRSTUVWX", 0, 104, 4);
+  tft.drawString(" !\"#!&$", 0, 0, 4);
+  tft.drawString("A p p l e f u c k e r s", 0, 26, 4);
+  tft.drawString("A p p l e f u c k e r s", 0, 52, 4);
+  tft.drawString("A p p l e f u c k e r s", 0, 78, 4);
+  tft.drawString("Applefuckers", 0, 104, 4);
 
   delay(WAIT);
   tft.fillScreen(TFT_BROWN);
-  tft.drawString("YZ[\\]^_`abc", 0, 0, 4);
-  tft.drawString("defghijklmno", 0, 26, 4);
-  tft.drawString("pqrstuvwxyz", 0, 52, 4);
+  tft.drawString("A", 0, 0, 4);
+  tft.drawString("P", 0, 26, 4);
+  tft.drawString("P", 0, 52, 4);
   xpos = 0;
-  xpos += tft.drawString("{|}~", 0, 78, 4);
+  xpos += tft.drawString("L", 0, 78, 4);
   tft.drawChar(127, xpos, 78, 4);
   delay(WAIT);
 
   tft.fillScreen(TFT_BROWN);
   tft.setTextColor(TFT_BLUE);
 
-  tft.drawString("012345", 0, 0, 6);
-  tft.drawString("6789", 0, 40, 6);
-  tft.drawString("apm-:.", 0, 80, 6);
+  tft.drawString("E", 0, 0, 6);
+  tft.drawString("f", 0, 40, 6);
+  tft.drawString("u", 0, 80, 6);
   delay(WAIT);
 
   tft.fillScreen(TFT_BROWN);
   tft.setTextColor(TFT_RED);
 
-  tft.drawString("0123", 0, 0, 7);
-  tft.drawString("4567", 0, 60, 7);
+  tft.drawString("c", 0, 0, 7);
+  tft.drawString("k", 0, 60, 7);
   delay(WAIT);
 
   tft.fillScreen(TFT_BROWN);
-  tft.drawString("890:.", 0, 0, 7);
+  tft.drawString("e", 0, 0, 7);
   tft.drawString("", 0, 60, 7);
   delay(WAIT);
 
   tft.fillScreen(TFT_BROWN);
   tft.setTextColor(TFT_YELLOW);
 
-  tft.drawString("0123", 0, 0, 8);
+  tft.drawString("r", 0, 0, 8);
   delay(WAIT);
 
   tft.fillScreen(TFT_BROWN);
-  tft.drawString("4567", 0, 0, 8);
+  tft.drawString("s", 0, 0, 8);
   delay(WAIT);
 
   tft.fillScreen(TFT_BROWN);
-  tft.drawString("890:.", 0, 0, 8);
+  tft.drawString("LFG!", 0, 0, 8);
   delay(WAIT);
 
   tft.setTextColor(TFT_MAGENTA);
